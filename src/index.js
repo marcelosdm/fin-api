f018489f6c605d2e8651b7e8eda25965a961fcfd
Marcelo Melo <marcelomelo.ciandt@sulamerica.com.br>
2021-10-02 16:42:56 -0300

    Add method to create a custumer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
+
+app.use(express.json());
+
+const custumers = [];
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+
+  const custumerAlreadyExists = custumers.some(
+    (custumer) => custumer.cpf === cpf
+  );
+
+  custumerAlreadyExists && response.status(400).json({error: "Custumer already exists"})
+
+
+  custumers.push({
+    cpf,
+    name,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
